400dc8dbf0f462f674064a28de6facd04d6ea074
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-07-01 08:39:59 +0100

    Extract removeHyperlinks() to its own file, so it can be tested
    Pure refactoring - done with the Move facility in WebStorm.

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { Editor, MarkdownView, Notice, Plugin } from 'obsidian';
+import { removeHyperlinks } from './removeHyperlinks';
 
 export default class HyperlinkRemover extends Plugin {
 	async onload() {
@@ -79,36 +80,3 @@ export default class HyperlinkRemover extends Plugin {
 	}
 }
 
-function removeHyperlinks(text:string): string {
-	let result = text;
-	let match;
-	const regex = /\[((?:[^\]\\]|\\.|\](?!\())*?)\]\(/g;
-
-	while ((match = regex.exec(text)) !== null) {
-		const linkText = match[1];
-		const startPos = match.index;
-		const urlStartPos = match.index + match[0].length;
-
-		// Find the matching closing parenthesis
-		let parenCount = 1;
-		let urlEndPos = urlStartPos;
-
-		while (urlEndPos < text.length && parenCount > 0) {
-			if (text[urlEndPos] === '(') {
-				parenCount++;
-			} else if (text[urlEndPos] === ')') {
-				parenCount--;
-			}
-			if (parenCount > 0) {
-				urlEndPos++;
-			}
-		}
-
-		if (parenCount === 0) {
-			const fullMatch = text.substring(startPos, urlEndPos + 1);
-			result = result.replace(fullMatch, linkText);
-		}
-	}
-
-	return result;
-}
