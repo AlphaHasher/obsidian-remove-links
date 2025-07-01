10d00bd5b5e3d162bb03b18e99ae57b662961889
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-07-01 08:42:11 +0100

    Make tests check the removeHyperlinks() that is used by main.ts
    Jest test now imports removeHyperlinks() used by main.ts,
    removing the original need to keep the two copies of
    removeHyperlinks() in sync.

--- removeHyperlinks.test.ts
+++ removeHyperlinks.test.ts
@@ -1,38 +1,6 @@
 import { describe, expect, test } from '@jest/globals';
 
-function removeHyperlinks(text:string): string {
-	let result = text;
-	let match;
-	const regex = /\[((?:[^\]\\]|\\.|\](?!\())*?)\]\(/g;
-
-	while ((match = regex.exec(text)) !== null) {
-		const linkText = match[1];
-		const startPos = match.index;
-		const urlStartPos = match.index + match[0].length;
-
-		// Find the matching closing parenthesis
-		let parenCount = 1;
-		let urlEndPos = urlStartPos;
-
-		while (urlEndPos < text.length && parenCount > 0) {
-			if (text[urlEndPos] === '(') {
-				parenCount++;
-			} else if (text[urlEndPos] === ')') {
-				parenCount--;
-			}
-			if (parenCount > 0) {
-				urlEndPos++;
-			}
-		}
-
-		if (parenCount === 0) {
-			const fullMatch = text.substring(startPos, urlEndPos + 1);
-			result = result.replace(fullMatch, linkText);
-		}
-	}
-
-	return result;
-}
+import { removeHyperlinks } from './removeHyperlinks';
 
 describe('Remove Hyper Links Tests', () => {
   test('remove hyperlinks from text', () => {
